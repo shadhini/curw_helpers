75cea794c51c450fe6686bffb7433cd347bce939
shadhini <jshadhiniaseka@gmail.com>
2019-05-20 10:13:06 +0530

    add script to generate rfield

---
+++ db_stored_procedures/curw_fcst/get_TS_start_end.sql
@@ -0,0 +1,17 @@
+CREATE DEFINER=`root`@`%` PROCEDURE `get_TS_start_end`(
+IN model VARCHAR(25),
+IN version VARCHAR(25)
+)
+BEGIN
+SET @sourceID = (SELECT curw_fcst.source.id FROM curw_fcst.source WHERE curw_fcst.source.model = model and curw_fcst.source.version = version);
+SET @fgt = (SELECT curw_fcst.run.end_date FROM curw_fcst.run WHERE curw_fcst.run.source = @sourceID limit 1);
+SET @random_id = (SELECT curw_fcst.run.id FROM curw_fcst.run WHERE curw_fcst.run.source = @sourceID limit 1);
+
+SELECT 
+    MIN(curw_fcst.data.time) as start, MAX(curw_fcst.data.time) as end
+FROM
+    curw_fcst.data
+WHERE
+    curw_fcst.data.id = @random_id
+        AND curw_fcst.data.fgt = @fgt;
+END
