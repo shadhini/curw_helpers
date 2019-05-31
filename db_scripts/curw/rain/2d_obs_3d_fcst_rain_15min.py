a940bff60ce297a3180050a9d5eaacfae6b92ac5
shadhini <jshadhiniaseka@gmail.com>
2019-05-31 12:33:18 +0530

    update script to prepare MIKE INPUT

--- db_scripts/curw/rain/2d_obs_3d_fcst_rain_15min.py
+++ db_scripts/curw/rain/2d_obs_3d_fcst_rain_15min.py
@@ -2,6 +2,7 @@ import traceback
 import pymysql
 from csv_utils import create_csv, read_csv, delete_row
 from datetime import datetime, timedelta
+from file_utils import create_csv_like_txt
 
 
 # All Rainfall Obs Stations :: variable=Precipitation, unit=mm|Accumulative, type=Observed
@@ -64,8 +65,8 @@ def extract_rain_obs(connection, stations_dict, start_time, end_time):
 
     except Exception as ex:
         traceback.print_exc()
-    finally:
-        connection.close()
+    # finally:
+    #     connection.close()
 
 
 def extract_wrf0_rain_fcst(connection, station_dict, start_time, end_time, type):
@@ -86,9 +87,9 @@ def extract_wrf0_rain_fcst(connection, station_dict, start_time, end_time, type)
         for station in station_dict.keys():
 
             with connection.cursor() as cursor1:
-                sql_statement = "select id from run where variable=1 and unit=1 and type=%d and source=8 and " \
-                                "station=%d;"
-                cursor1.execute(sql_statement, (type, station_dict.get(station)))
+                sql_statement = "select id from run where name='Cloud-1' and variable=1 and unit=1  and source=8 and " \
+                                "station=%s and type=%s;"
+                cursor1.execute(sql_statement, (station_dict.get(station), type))
                 id = cursor1.fetchone()['id']
 
             with connection.cursor() as cursor2:
@@ -100,54 +101,70 @@ def extract_wrf0_rain_fcst(connection, station_dict, start_time, end_time, type)
                 for result in results:
                     ts.append([result.get('time'), result.get('value')])
 
-            forecast_timeseries[station] = ts
+                forecast_timeseries[station] = ts
+
+        for key in forecast_timeseries.keys():
+            print(key, "#########", forecast_timeseries.get(key))
 
         return forecast_timeseries
 
     except Exception as ex:
         traceback.print_exc()
-    finally:
-        connection.close()
+    # finally:
+    #     connection.close()
 
 
 def extract_active_rainfall_obs_stations():
     return
 
 
-def extract_15_min_timeseries(timeseries, MIKE_INPUT, current_row):
+def extract_15_min_timeseries(timeseries, MIKE_INPUT, current_row, index):
 
     dict = {}
 
     MIKE_INPUT = MIKE_INPUT
     current_row = current_row
 
-    if (timeseries[1][0] + timedelta(hours=1)).strftime('%Y-%m-%d %H:%M:%S')==timeseries[2][0]:  # hourly series
-        for i in range(len(timeseries)):
-            avg = timeseries[i][1] / 4
-            avg_next = timeseries[i + 1][1] / 4
-            if MIKE_INPUT[current_row][0]==timeseries[i][0]:
+    if (timeseries[0][0] + timedelta(hours=1))==timeseries[1][0]:  # hourly series
+        print("inside hourly series")
+        for i in range(len(timeseries)-1):
+            if len(MIKE_INPUT[current_row]) == index:  # skip overwriting existing values
+                avg = timeseries[i][1] / 4
+                avg_next = timeseries[i + 1][1] / 4
+                if MIKE_INPUT[current_row][0]==(timeseries[i][0]).strftime('%Y-%m-%d %H:%M:%S'):
+                    MIKE_INPUT[current_row].append(avg)
+                    MIKE_INPUT[current_row + 1].append(avg_next)
+                    MIKE_INPUT[current_row + 2].append(avg_next)
+                    MIKE_INPUT[current_row + 3].append(avg_next)
+                    current_row = current_row + 4
+                else:  # time entry is not matching
+                    continue
+            else:
+                current_row +=1
+
+        if len(MIKE_INPUT[current_row])==index:  # skip overwriting existing values
+            avg = timeseries[len(timeseries)-1][1] / 4
+            if MIKE_INPUT[current_row][0]==(timeseries[len(timeseries)-1][0]).strftime('%Y-%m-%d %H:%M:%S'):
                 MIKE_INPUT[current_row].append(avg)
-                MIKE_INPUT[current_row + 1].append(avg_next)
-                MIKE_INPUT[current_row + 2].append(avg_next)
-                MIKE_INPUT[current_row + 3].append(avg_next)
-                current_row = current_row + 4
-            elif MIKE_INPUT[current_row][0] < timeseries[i][0]:  # handle missing data with appending ''
-                MIKE_INPUT[current_row].append('')
                 current_row += 1
-            else:  # avoid rewriting existing fields
-                current_row += 1
-                continue
-    elif (timeseries[1][0] + timedelta(minutes=15)).strftime('%Y-%m-%d %H:%M:%S')==timeseries[2][0]:  # 15 min periodical series
+        else:
+            current_row += 1
+
+    elif (timeseries[1][0] + timedelta(minutes=15))==timeseries[2][0]:  # 15 min periodical series
+        print("Inside 15 min series")
         for i in range(len(timeseries)):
-            if MIKE_INPUT[current_row][0]==timeseries[i][0]:
-                MIKE_INPUT[current_row].append(timeseries[i][1])
+            if len(MIKE_INPUT[current_row])==index:  # skip overwriting existing values
+                print(MIKE_INPUT[current_row][0])
+                print((timeseries[i][0]).strftime('%Y-%m-%d %H:%M:%S'))
+                if MIKE_INPUT[current_row][0]==(timeseries[i][0]).strftime('%Y-%m-%d %H:%M:%S'):
+                    print("Inside case 1")
+                    MIKE_INPUT[current_row].append(timeseries[i][1])
+                    current_row += 1
+                else:  # time entry is not matching
+                    print('inside case 2')
+                    continue
+            else:
                 current_row += 1
-            elif MIKE_INPUT[current_row][0] < timeseries[i][0]:  # handle missing data with appending ''
-                MIKE_INPUT[current_row].append('')
-                current_row += 1
-            else:  # avoid rewriting existing fields
-                current_row += 1
-                continue
 
     dict['MIKE_INPUT'] = MIKE_INPUT
     dict['current_row'] = current_row
@@ -160,89 +177,120 @@ def generate_mike_input(active_obs_stations_file, obs_wrf0_mapping_file):
     types = [16, 17, 18]
     now = datetime.now()
     obs_start = (now - timedelta(days=2)).strftime('%Y-%m-%d 00:00:00')
-    obs_end = now.strftime('%Y-%m-%d 00:00:00')
-    d0_forecast_start = (now - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
-    d0_forecast_end = (now + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+    obs_end = now.strftime('%Y-%m-%d %H:%M:%S')
+    d0_forecast_start = (now - timedelta(hours=12)).strftime('%Y-%m-%d %H:%M:%S')
+    d0_forecast_end = (now + timedelta(days=1)).strftime('%Y-%m-%d 00:00:00')
     d1_forecast_start = d0_forecast_end
-    d1_forecast_end = (now + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S')
+    d1_forecast_end = (now + timedelta(days=2)).strftime('%Y-%m-%d 00:00:00')
     d2_forecast_start = d1_forecast_end
-    d2_forecast_end = (now + timedelta(days=3)).strftime('%Y-%m-%d %H:%M:%S')
+    d2_forecast_end = (now + timedelta(days=3)).strftime('%Y-%m-%d 00:00:00')
 
-    # Connect to the database
-    connection = pymysql.connect(host='104.198.0.87',
-            user='root',
-            password='cfcwm07',
-            db='curw',
-            charset='utf8mb4',
-            cursorclass=pymysql.cursors.DictCursor)
+    try:
+        # Connect to the database
+        connection = pymysql.connect(host='104.198.0.87',
+                user='root',
+                password='cfcwm07',
+                db='curw',
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor)
 
-    active_obs_stations = read_csv(active_obs_stations_file)
-    obs_wrf0_mapping = read_csv(obs_wrf0_mapping_file)
+        print("{} : Read active obs stations and obs wrf0 station mapping from CSVs".format(datetime.now()))
+        active_obs_stations = read_csv(active_obs_stations_file)
+        obs_wrf0_mapping = read_csv(obs_wrf0_mapping_file)
 
-    stations_dict_for_obs={}  # keys: obs station id , value: hash id
+        stations_dict_for_obs={}  # keys: obs station id , value: hash id
 
-    for obs_index in range(len(active_obs_stations)):
-        stations_dict_for_obs[active_obs_stations[obs_index][2]] = active_obs_stations[obs_index][0]
+        for obs_index in range(len(active_obs_stations)):
+            stations_dict_for_obs[active_obs_stations[obs_index][2]] = active_obs_stations[obs_index][0]
 
-    obs_timeseries = extract_rain_obs(connection=connection, stations_dict=stations_dict_for_obs,
-            start_time=obs_start, end_time=obs_end)
+        print("{} : Pull obs timeseries from db".format(datetime.now()))
+        obs_timeseries = extract_rain_obs(connection=connection, stations_dict=stations_dict_for_obs,
+                start_time=obs_start, end_time=obs_end)
 
-    stations_dict_for_fcst={}  # keys: obs station id , value: nearest wrf0 station id
+        stations_dict_for_fcst={}  # keys: obs station id , value: nearest wrf0 station id
 
-    for obs_index in range(len(obs_wrf0_mapping)):
-        stations_dict_for_fcst[obs_wrf0_mapping[obs_index][0]] = obs_wrf0_mapping[obs_index][2]
+        for obs_index in range(len(obs_wrf0_mapping)):
+            stations_dict_for_fcst[obs_wrf0_mapping[obs_index][0]] = obs_wrf0_mapping[obs_index][2]
 
-    d0_wrf0_fcst_timeseries = extract_wrf0_rain_fcst(connection=connection, station_dict=stations_dict_for_fcst,
-            start_time=d0_forecast_start, end_time=d0_forecast_end, type=16)
+        print("{} : Pull 0-d forecast timeseries from db".format(datetime.now()))
+        d0_wrf0_fcst_timeseries = extract_wrf0_rain_fcst(connection=connection, station_dict=stations_dict_for_fcst,
+                start_time=d0_forecast_start, end_time=d0_forecast_end, type=16)
 
-    d1_wrf0_fcst_timeseries = extract_wrf0_rain_fcst(connection=connection, station_dict=stations_dict_for_fcst,
-            start_time=d1_forecast_start, end_time=d1_forecast_end, type=17)
+        print("{} : Pull 1-d forecast timeseries from db".format(datetime.now()))
+        d1_wrf0_fcst_timeseries = extract_wrf0_rain_fcst(connection=connection, station_dict=stations_dict_for_fcst,
+                start_time=d1_forecast_start, end_time=d1_forecast_end, type=17)
 
-    d2_wrf0_fcst_timeseries = extract_wrf0_rain_fcst(connection=connection, station_dict=stations_dict_for_fcst,
-            start_time=d2_forecast_start, end_time=d2_forecast_end, type=18)
+        print("{} : Pull 2-d forecast timeseries from db".format(datetime.now()))
+        d2_wrf0_fcst_timeseries = extract_wrf0_rain_fcst(connection=connection, station_dict=stations_dict_for_fcst,
+                start_time=d2_forecast_start, end_time=d2_forecast_end, type=18)
 
-    # format MIKE input
-    MIKE_INPUT = [['time']]
+        # format MIKE input
+        MIKE_INPUT = [['time']]
 
-    ordered_station_ids = []
-    for obs_index in range(len(obs_wrf0_mapping)):
-        MIKE_INPUT[0].append(obs_wrf0_mapping[obs_index][1])
-        ordered_station_ids.append(obs_wrf0_mapping[obs_index][0])
+        print("{} : Initialize MIKE INPUT".format(datetime.now()))
+        ordered_station_ids = []
+        for obs_index in range(len(obs_wrf0_mapping)):
+            MIKE_INPUT[0].append(obs_wrf0_mapping[obs_index][1])
+            ordered_station_ids.append(obs_wrf0_mapping[obs_index][0])
 
-    timestamp = obs_start
-    while timestamp <= d2_forecast_end :
-        MIKE_INPUT.append([timestamp])
-        timestamp = (timestamp + timedelta(minutes=15)).strftime('%Y-%m-%d %H:%M:%S')
+        timestamp = obs_start
+        while timestamp <= d2_forecast_end :
+            MIKE_INPUT.append([timestamp])
+            timestamp = (datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S') + timedelta(minutes=15)).strftime('%Y-%m-%d %H:%M:%S')
 
-    for column in range(len(ordered_station_ids)):
-        obs = obs_timeseries.get(ordered_station_ids[column])
-        d0_wrf0_fcst = d0_wrf0_fcst_timeseries.get(ordered_station_ids[column])
-        d1_wrf1_fcst = d1_wrf0_fcst_timeseries.get(ordered_station_ids[column])
-        d2_wrf2_fcst = d2_wrf0_fcst_timeseries.get(ordered_station_ids[column])
+        # for column in range(len(ordered_station_ids)):
+        for column in range(1):
+            obs = obs_timeseries.get(ordered_station_ids[column])
+            d0_wrf0_fcst = d0_wrf0_fcst_timeseries.get(ordered_station_ids[column])
+            d1_wrf0_fcst = d1_wrf0_fcst_timeseries.get(ordered_station_ids[column])
+            d2_wrf0_fcst = d2_wrf0_fcst_timeseries.get(ordered_station_ids[column])
 
-        current_row = 0
+            print('obs tmeseries: ', obs)
+            current_row = 1
 
-        obs_output_dict = extract_15_min_timeseries(timeseries=obs, MIKE_INPUT=MIKE_INPUT, current_row=current_row)
+            print("{} : Add obs timeseries to MIKE INPUT".format(datetime.now()))
+            if obs is not None and len(obs) > 0:
+                obs_output_dict = extract_15_min_timeseries(timeseries=obs, MIKE_INPUT=MIKE_INPUT,
+                        current_row=current_row, index=column+1)
 
-        MIKE_INPUT = obs_output_dict.get('MIKE_INPUT')
-        current_row = obs_output_dict.get('current_row')
+            MIKE_INPUT = obs_output_dict.get('MIKE_INPUT')
+            current_row = obs_output_dict.get('current_row')
 
-        d0_fcst_output_dict = extract_15_min_timeseries(timeseries=d0_wrf0_fcst, MIKE_INPUT=MIKE_INPUT, current_row=current_row)
+            print("{} : Add 0-d forecast timeseries to MIKE INPUT".format(datetime.now()))
+            if d0_wrf0_fcst is not None and len(d0_wrf0_fcst) > 0:
+                d0_fcst_output_dict = extract_15_min_timeseries(timeseries=d0_wrf0_fcst, MIKE_INPUT=MIKE_INPUT,
+                        current_row=current_row, index=column+1)
 
-        MIKE_INPUT = d0_fcst_output_dict.get('MIKE_INPUT')
-        current_row = d0_fcst_output_dict.get('current_row')
+            MIKE_INPUT = d0_fcst_output_dict.get('MIKE_INPUT')
+            current_row = d0_fcst_output_dict.get('current_row')
 
-        d1_fcst_output_dict = extract_15_min_timeseries(timeseries=d1_wrf1_fcst, MIKE_INPUT=MIKE_INPUT, current_row=current_row)
+            print("{} : Add 1-d forecast timeseries to MIKE INPUT".format(datetime.now()))
+            if d1_wrf0_fcst is not None and len(d1_wrf0_fcst) > 0:
+                d1_fcst_output_dict = extract_15_min_timeseries(timeseries=d1_wrf0_fcst, MIKE_INPUT=MIKE_INPUT,
+                        current_row=current_row, index=column+1)
 
-        MIKE_INPUT = d1_fcst_output_dict.get('MIKE_INPUT')
-        current_row = d1_fcst_output_dict.get('current_row')
+            MIKE_INPUT = d1_fcst_output_dict.get('MIKE_INPUT')
+            current_row = d1_fcst_output_dict.get('current_row')
 
-        d2_fcst_output_dict = extract_15_min_timeseries(timeseries=d2_wrf2_fcst, MIKE_INPUT=MIKE_INPUT, current_row=current_row)
+            print("{} : Add 2-d forecast timeseries to MIKE INPUT".format(datetime.now()))
+            if d2_wrf0_fcst is not None and len(d2_wrf0_fcst) > 0:
+                d2_fcst_output_dict = extract_15_min_timeseries(timeseries=d2_wrf0_fcst, MIKE_INPUT=MIKE_INPUT,
+                        current_row=current_row, index=column+1)
 
-        MIKE_INPUT = d2_fcst_output_dict.get('MIKE_INPUT')
-        current_row = d2_fcst_output_dict.get('current_row')
+            MIKE_INPUT = d2_fcst_output_dict.get('MIKE_INPUT')
+            current_row = d2_fcst_output_dict.get('current_row')
 
-    create_csv('mike_kelani_{}_{}'.format(now.strftime('%Y-%m-%d_%H-00-00')), MIKE_INPUT)
+            for i in range(len(MIKE_INPUT)-1):
+                if len(MIKE_INPUT[i+1]) < (column + 2):
+                    for j in range((column + 2) - len(MIKE_INPUT[i+1])):
+                        MIKE_INPUT[i+1].append('')
+
+        create_csv_like_txt('mike_kelani_{}.txt'.format(now.strftime('%Y-%m-%d_%H-00-00')), MIKE_INPUT)
+
+    except Exception as e:
+        traceback.print_exc()
+    finally:
+        connection.close()
 
 
 def generate_rain_files(active_obs_stations_file, start_time, end_time):
@@ -274,3 +322,6 @@ def generate_rain_files(active_obs_stations_file, start_time, end_time):
 
 
 # generate_rain_files('active_rainfall_obs_stations.csv', "2019-05-22 23:45:00", "2019-05-25 23:30:00")
+
+generate_mike_input('active_rainfall_obs_stations.csv', 'obs_wrf0_stations_mapping.csv')
+
