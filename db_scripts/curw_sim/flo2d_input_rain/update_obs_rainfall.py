dde6b5059670f47802bbaf98a180f32fcb6e4a95
shadhini <jshadhiniaseka@gmail.com>
2019-06-07 06:39:47 +0530

    test and debug script to push observed rainfall data to curw_sim

--- db_scripts/curw_sim/flo2d_input_rain/update_obs_rainfall.py
+++ db_scripts/curw_sim/flo2d_input_rain/update_obs_rainfall.py
@@ -1,14 +1,26 @@
 import traceback
 import pymysql
+from datetime import datetime, timedelta
+import csv
 
 from db_adapter.base import get_Pool
 from db_adapter.constants import CURW_SIM_DATABASE, CURW_SIM_PASSWORD, CURW_SIM_USERNAME, CURW_SIM_PORT, CURW_SIM_HOST
-from db_adapter.curw_sim.grids.grid_utils import get_flo2d_to_obs_grid_mappings
-from db_adapter.curw_sim.timeseries import  Timeseries
+from db_adapter.curw_sim.grids import get_flo2d_to_obs_grid_mappings, GridInterpolationEnum, \
+    FLO2D_250, FLO2D_150, FLO2D_30
+from db_adapter.curw_sim.timeseries import  Timeseries, MethodEnum
 
-from csv_utils import create_csv, read_csv, delete_row
-from datetime import datetime, timedelta
-from file_utils import create_csv_like_txt
+
+def read_csv(file_name):
+    """
+    Read csv file
+    :param file_name: <file_path/file_name>.csv
+    :return: list of lists which contains each row of the csv file
+    """
+
+    with open(file_name, 'r') as f:
+        data = [list(line) for line in csv.reader(f)][1:]
+
+    return data
 
 
 def extract_rain_ts(connection, id, start_time):
@@ -23,24 +35,24 @@ def extract_rain_ts(connection, id, start_time):
     timeseries = []
 
     try:
-
-
         # Extract per 5 min observed timeseries
         with connection.cursor() as cursor1:
             sql_statement = "select `time`, `value`  from data where `id`=%s and `time` > %s ;"
+            print(id, start_time)
             rows = cursor1.execute(sql_statement, (id, start_time))
             if rows > 0:
                 results = cursor1.fetchall()
                 for result in results:
                     timeseries.append([result.get('time'), result.get('value')])
-
+        if len(timeseries) > 1:
+            print(id, "::", len(timeseries), "::", timeseries[1][0], "::", timeseries[-1][0])
         return timeseries
 
     except Exception as ex:
         traceback.print_exc()
-    finally:
-        if connection is not None:
-            connection.close()
+    # finally:
+    #     if connection is not None:
+    #         connection.close()
 
 
 # for bulk insertion for a given one grid interpolation method
@@ -54,8 +66,10 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation):
     :return:
     """
 
+    print("Update rainfall observations")
+
     now = datetime.now()
-    obs_start = (now - timedelta(hours=12)).strftime('%Y-%m-%d %H:%M:%S')
+    OBS_START = (now - timedelta(hours=12)).strftime('%Y-%m-%d %H:%M:%S')
 
     try:
 
@@ -72,7 +86,6 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation):
 
         TS = Timeseries(pool=pool)
 
-        print("{} : Load active curw rainfall obs stations and their hash ids from CSV".format(datetime.now()))
         active_obs_stations = read_csv('curw_active_rainfall_obs_stations.csv')
         flo2d_grids = read_csv('{}m.csv'.format(flo2d_model))
 
@@ -84,8 +97,9 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation):
         flo2d_obs_mapping = get_flo2d_to_obs_grid_mappings(pool=pool, grid_interpolation=grid_interpolation, flo2d_model=flo2d_model)
 
         for flo2d_index in range(len(flo2d_grids)):
+            obs_start = OBS_START
             meta_data = {
-                    'latitude': '%.6f' % flo2d_grids[flo2d_index][2], 'longitude': '%.6f' % flo2d_grids[flo2d_index][1],
+                    'latitude': float('%.6f' % float(flo2d_grids[flo2d_index][2])), 'longitude': float('%.6f' % float(flo2d_grids[flo2d_index][1])),
                     'model': flo2d_model, 'method': method,
                     'grid_id': '{}_{}_{}'.format(flo2d_model, flo2d_grids[flo2d_index][0], grid_interpolation)
                     }
@@ -95,6 +109,7 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation):
             if tms_id is None:
                 tms_id = TS.generate_timeseries_id(meta_data=meta_data)
                 meta_data['id'] = tms_id
+                print("{} : Insert entry to run table with id={}".format(datetime.now(), tms_id))
                 TS.insert_run(meta_data=meta_data)
 
             obs_end = TS.get_obs_end(id_=tms_id)
@@ -102,30 +117,50 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation):
             if obs_end is not None:
                 obs_start = obs_end
 
-            obs1_hash_id = stations_dict_for_obs.get(flo2d_obs_mapping.get(meta_data['grid_id'])[0])
-            obs2_hash_id = stations_dict_for_obs.get(flo2d_obs_mapping.get(meta_data['grid_id'])[1])
-            obs3_hash_id = stations_dict_for_obs.get(flo2d_obs_mapping.get(meta_data['grid_id'])[2])
+            obs1_hash_id = stations_dict_for_obs.get(str(flo2d_obs_mapping.get(meta_data['grid_id'])[0]))
+            obs2_hash_id = stations_dict_for_obs.get(str(flo2d_obs_mapping.get(meta_data['grid_id'])[1]))
+            obs3_hash_id = stations_dict_for_obs.get(str(flo2d_obs_mapping.get(meta_data['grid_id'])[2]))
+
+            obs_timeseries = []
 
             ts = extract_rain_ts(connection=curw_connection, start_time=obs_start, id=obs1_hash_id)
-            obs_timeseries = ts[1:]
-            ts2 = extract_rain_ts(connection=curw_connection, start_time=ts[-1][0], id=obs2_hash_id)
-            if len(ts2) > 1:
-                obs_timeseries.extend(ts2[1:])
-            ts3 = extract_rain_ts(connection=curw_connection, start_time=ts2[-1][0], id=obs3_hash_id)
-            if len(ts3) > 1:
-                obs_timeseries.extend(ts3[1:])
+            if ts is not None and len(ts) > 1:
+                obs_timeseries.extend(ts[1:])
+                print("{} : Extracted timeseries from obs station 1 till {}".format(datetime.now(), ts[-1][0]))
+                obs_start = ts[-1][0]
 
+            ts2 = extract_rain_ts(connection=curw_connection, start_time=obs_start, id=obs2_hash_id)
+            if ts2 is not None and len(ts2) > 1:
+                obs_timeseries.extend(ts2[1:])
+                print("{} : Extracted timeseries from obs station 2 till {}".format(datetime.now(), ts2[-1][0]))
+                obs_start = ts2[-1][0]
+
+            ts3 = extract_rain_ts(connection=curw_connection, start_time=obs_start, id=obs3_hash_id)
+            if ts3 is not None and len(ts3) > 1:
+                obs_timeseries.extend(ts3[1:])
+                print("{} : Extracted timeseries from obs station 3 till {}".format(datetime.now(), ts3[-1][0]))
+
+            print("{} : Insert timeseries to database".format(datetime.now()))
             TS.insert_data(timeseries=obs_timeseries, tms_id=tms_id, upsert=True)
 
-            TS.update_latest_obs(id_=tms_id, obs_end=obs_timeseries[-1][0])
+            if obs_timeseries is not None and len(obs_timeseries) > 0:
+                print("{} : Update latest obs {}".format(datetime.now(), obs_timeseries[-1][1]))
+                TS.update_latest_obs(id_=tms_id, obs_end=(obs_timeseries[-1][1]))
 
     except Exception as e:
         traceback.print_exc()
     finally:
-        if curw_connection is not None:
-            curw_connection.close()
+        pool.destroy()
 
 
+method = MethodEnum.getAbbreviation(MethodEnum.MME)
+grid_interpolation = GridInterpolationEnum.getAbbreviation(GridInterpolationEnum.MDPA)
 
+print("{} : ####### Insert obs rainfall for FLO2D 250 grids")
+update_rainfall_obs(flo2d_model=FLO2D_250, method=method, grid_interpolation=grid_interpolation)
 
+print("{} : ####### Insert obs rainfall for FLO2D 150 grids")
+update_rainfall_obs(flo2d_model=FLO2D_150, method=method, grid_interpolation=grid_interpolation)
 
+print("{} : ####### Insert obs rainfall for FLO2D 30 grids")
+update_rainfall_obs(flo2d_model=FLO2D_30, method=method, grid_interpolation=grid_interpolation)
--- db_scripts/curw_sim/grid_map_utils.py
+++ db_scripts/curw_sim/grid_map_utils.py
@@ -1,3 +1,4 @@
+import traceback
 from db_adapter.base import get_Pool
 from db_adapter.curw_sim.grids import add_flo2d_grid_mappings, get_flo2d_to_obs_grid_mappings, \
     get_flo2d_to_wrf_grid_mappings, GridInterpolationEnum
@@ -5,25 +6,30 @@ from db_adapter.constants import CURW_SIM_HOST, CURW_SIM_PORT, CURW_SIM_USERNAME
 
 print(" Add flo2d grid mappings")
 
-pool = get_Pool(host=CURW_SIM_HOST, port=CURW_SIM_PORT, user=CURW_SIM_USERNAME, password=CURW_SIM_PASSWORD, db=CURW_SIM_DATABASE)
+try:
 
-grid_interpolation_method = GridInterpolationEnum.getAbbreviation(GridInterpolationEnum.MDPA)
+    pool = get_Pool(host=CURW_SIM_HOST, port=CURW_SIM_PORT, user=CURW_SIM_USERNAME, password=CURW_SIM_PASSWORD, db=CURW_SIM_DATABASE)
 
-print(" Add flo2d 250 grid mappings")
-add_flo2d_grid_mappings(pool=pool, flo2d_model='flo2d_250', grid_interpolation=grid_interpolation_method)
-print("{} flo2d 250 grids added".format(len(get_flo2d_to_wrf_grid_mappings(pool=pool, flo2d_model='flo2d_250', grid_interpolation=grid_interpolation_method).keys())))
-print("{} flo2d 250 grids added".format(len(get_flo2d_to_obs_grid_mappings(pool=pool, flo2d_model='flo2d_250', grid_interpolation=grid_interpolation_method).keys())))
+    grid_interpolation_method = GridInterpolationEnum.getAbbreviation(GridInterpolationEnum.MDPA)
+
+    print(" Add flo2d 250 grid mappings")
+    add_flo2d_grid_mappings(pool=pool, flo2d_model='flo2d_250', grid_interpolation=grid_interpolation_method)
+    print("{} flo2d 250 grids added".format(len(get_flo2d_to_wrf_grid_mappings(pool=pool, flo2d_model='flo2d_250', grid_interpolation=grid_interpolation_method).keys())))
+    print("{} flo2d 250 grids added".format(len(get_flo2d_to_obs_grid_mappings(pool=pool, flo2d_model='flo2d_250', grid_interpolation=grid_interpolation_method).keys())))
 
 
-print(" Add flo2d 150 grid mappings")
-add_flo2d_grid_mappings(pool=pool, flo2d_model='flo2d_150', grid_interpolation=grid_interpolation_method)
-print("{} flo2d 150 grids added".format(len(get_flo2d_to_wrf_grid_mappings(pool=pool, flo2d_model='flo2d_150', grid_interpolation=grid_interpolation_method).keys())))
-print("{} flo2d 150 grids added".format(len(get_flo2d_to_obs_grid_mappings(pool=pool, flo2d_model='flo2d_150', grid_interpolation=grid_interpolation_method).keys())))
+    print(" Add flo2d 150 grid mappings")
+    add_flo2d_grid_mappings(pool=pool, flo2d_model='flo2d_150', grid_interpolation=grid_interpolation_method)
+    print("{} flo2d 150 grids added".format(len(get_flo2d_to_wrf_grid_mappings(pool=pool, flo2d_model='flo2d_150', grid_interpolation=grid_interpolation_method).keys())))
+    print("{} flo2d 150 grids added".format(len(get_flo2d_to_obs_grid_mappings(pool=pool, flo2d_model='flo2d_150', grid_interpolation=grid_interpolation_method).keys())))
 
-print(" Add flo2d 30 grid mappings")
-add_flo2d_grid_mappings(pool=pool, flo2d_model='flo2d_30', grid_interpolation=grid_interpolation_method)
-print("{} flo2d 30 grids added".format(len(get_flo2d_to_wrf_grid_mappings(pool=pool, flo2d_model='flo2d_30', grid_interpolation=grid_interpolation_method).keys())))
-print("{} flo2d 30 grids added".format(len(get_flo2d_to_obs_grid_mappings(pool=pool, flo2d_model='flo2d_30', grid_interpolation=grid_interpolation_method).keys())))
+    print(" Add flo2d 30 grid mappings")
+    add_flo2d_grid_mappings(pool=pool, flo2d_model='flo2d_30', grid_interpolation=grid_interpolation_method)
+    print("{} flo2d 30 grids added".format(len(get_flo2d_to_wrf_grid_mappings(pool=pool, flo2d_model='flo2d_30', grid_interpolation=grid_interpolation_method).keys())))
+    print("{} flo2d 30 grids added".format(len(get_flo2d_to_obs_grid_mappings(pool=pool, flo2d_model='flo2d_30', grid_interpolation=grid_interpolation_method).keys())))
 
-pool.destroy()
-
+except Exception as e:
+    traceback.print_exc()
+finally:
+    pool.destroy()
+    print("Process Finished.")
