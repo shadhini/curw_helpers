a971d3038aa7ba41bb809264a9606a8a921dfe23
shadhini <jshadhiniaseka@gmail.com>
2019-09-30 22:52:31 +0530

    add stored procedure to retrieve timeseries start and end of a given wrf model for a given fgt

---
+++ db_stored_procedures/curw_fcst/get_wrf_TS_start_end_given_fgt.sql
@@ -0,0 +1,19 @@
+CREATE DEFINER=`routine_user`@`%` PROCEDURE `get_wrf_TS_start_end_given_fgt`(
+IN model VARCHAR(25),
+IN version VARCHAR(25),
+IN sim_tag VARCHAR(100),
+IN fgt VARCHAR(25)
+)
+BEGIN
+SET @sourceID = (SELECT curw_fcst.source.id FROM curw_fcst.source WHERE curw_fcst.source.model = model and curw_fcst.source.version = version);
+SET @random_id = (SELECT curw_fcst.run.id FROM curw_fcst.run WHERE curw_fcst.run.source = @sourceID AND curw_fcst.run.sim_tag=sim_tag limit 1);
+
+SELECT 
+    MIN(curw_fcst.data.time) AS start,
+    MAX(curw_fcst.data.time) AS end
+FROM
+    curw_fcst.data
+WHERE
+    curw_fcst.data.id = @random_id
+        AND curw_fcst.data.fgt LIKE fgt;
+END
