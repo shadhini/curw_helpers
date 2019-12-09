61651e58d1cc536cd6c9cd5f3362d86d1c6cc355
shadhini <jshadhiniaseka@gmail.com>
2019-12-09 15:28:57 +0530

    update procedure getNearestFcstWLforGivenStation

--- db_stored_procedures/curw_fcst/getNearestFcstWLforGivenStation.sql
+++ db_stored_procedures/curw_fcst/getNearestFcstWLforGivenStation.sql
@@ -10,13 +10,14 @@ BEGIN
 	SET @sourceID = (SELECT curw_fcst.source.id FROM curw_fcst.source WHERE curw_fcst.source.model = model and curw_fcst.source.version = version);
 	SET @tmsID = (SELECT curw_fcst.run.id FROM curw_fcst.run WHERE curw_fcst.run.source=@sourceID  AND curw_fcst.run.station=station_id 
 		AND curw_fcst.run.sim_tag=sim_tag AND curw_fcst.run.variable=2 AND curw_fcst.run.unit=2);
+	
 	SET @fgt = (SELECT sorted.fgt FROM (SELECT
 	temp.fgt,
     abs(expected_fgt - temp.fgt) as gap
 	FROM 
-    (select distinct curw_fcst.data.fgt as fgt from curw_fcst.data where curw_fcst.data.id=id) as temp
+    (select distinct curw_fcst.data.fgt as fgt from curw_fcst.data where curw_fcst.data.id=@tmsID) as temp
     order by gap limit 1) as sorted);
-        
+    
 SELECT 
     curw_fcst.data.time AS time,
     curw_fcst.data.fgt AS fgt,
@@ -28,4 +29,5 @@ WHERE
         AND curw_fcst.data.time >= start_time
         AND curw_fcst.data.fgt = @fgt
 ORDER BY time; 
+
 END
