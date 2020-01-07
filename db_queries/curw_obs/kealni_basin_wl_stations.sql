4a6a1a6e4eb2f2ea9df982886329b30778dfb594
shadhini <jshadhiniaseka@gmail.com>
2020-01-07 11:11:13 +0530

    add query to retrieve lat lon values of active waterlevel stations within kelani basin

---
+++ db_queries/curw_obs/kealni_basin_wl_stations.sql
@@ -0,0 +1,20 @@
+SELECT
+	selected_runs.id as hash_id,
+    curw_obs.station.id as station_id,
+    curw_obs.station.name as station_name,
+    curw_obs.station.latitude as latitude,
+    curw_obs.station.longitude as longitude
+FROM
+	(SELECT *
+    FROM
+    curw_obs.run
+    WHERE
+		curw_obs.run.variable = 17
+        -- AND curw_obs.run.unit = 9
+        AND end_date > SUBDATE(CURDATE(), INTERVAL 1 DAY)) AS selected_runs
+	LEFT JOIN
+		curw_obs.station ON curw_obs.station.id = selected_runs.station
+	WHERE
+		-- curw_obs.station.station_type = 'CUrW_WeatherStation' and
+        curw_obs.station.latitude BETWEEN 6.6 AND 7.4 and
+        curw_obs.station.longitude BETWEEN 79.6 AND 81.0;
