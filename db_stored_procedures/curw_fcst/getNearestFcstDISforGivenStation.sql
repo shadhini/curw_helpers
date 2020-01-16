b3fdc6f4052bc17e30b00af16ebf48117c2d5e87
shadhini <jshadhiniaseka@gmail.com>
2020-01-16 14:34:10 +0530

    add procedure to retrieve discharge data from database for given mode, station and fgt

---
+++ db_stored_procedures/curw_fcst/getNearestFcstDISforGivenStation.sql
@@ -0,0 +1,33 @@
+CREATE DEFINER=`routine_user`@`%` PROCEDURE `getNearestFcstDISforGivenStation`(
+IN model VARCHAR(25),
+IN version VARCHAR(25),
+IN station_id INT(11),
+IN sim_tag VARCHAR(100),
+IN expected_fgt DATETIME,
+IN start_time DATETIME
+)
+BEGIN
+	SET @sourceID = (SELECT curw_fcst.source.id FROM curw_fcst.source WHERE curw_fcst.source.model = model and curw_fcst.source.version = version);
+	SET @tmsID = (SELECT curw_fcst.run.id FROM curw_fcst.run WHERE curw_fcst.run.source=@sourceID  AND curw_fcst.run.station=station_id 
+		AND curw_fcst.run.sim_tag=sim_tag AND curw_fcst.run.variable=3 AND curw_fcst.run.unit=3);
+	
+	SET @fgt = (SELECT sorted.fgt FROM (SELECT
+	temp.fgt,
+    abs(expected_fgt - temp.fgt) as gap
+	FROM 
+    (select distinct curw_fcst.data.fgt as fgt from curw_fcst.data where curw_fcst.data.id=@tmsID) as temp
+    order by gap limit 1) as sorted);
+    
+SELECT 
+    curw_fcst.data.time AS time,
+    curw_fcst.data.fgt AS fgt,
+    curw_fcst.data.value AS value
+FROM
+    curw_fcst.data
+WHERE
+    curw_fcst.data.id = @tmsID
+        AND curw_fcst.data.time >= start_time
+        AND curw_fcst.data.fgt = @fgt
+ORDER BY time; 
+
+END
