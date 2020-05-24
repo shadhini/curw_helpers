4af2e8d4ebdd8261c0f4e9b41116324054a13d9e
shadhini <jshadhiniaseka@gmail.com>
2020-05-24 16:07:12 +0530

    test and debug stored procedure

--- db_triggers/curw_fcst/waterlevel_alert.sql
+++ db_triggers/curw_fcst/waterlevel_alert.sql
@@ -8,13 +8,13 @@ BEGIN
 	DECLARE station_name varchar(45);
     
     DECLARE finished INTEGER DEFAULT 0;
-    	DECLARE time DATETIME;
-    	DECLARE value DECIMAL(8,3);
+    	DECLARE tWL DATETIME;
+    	DECLARE vWL DECIMAL(8,3);
 
 	-- declare cursor for employee email
 	DEClARE curWL 
 		CURSOR FOR 
-			SELECT time, value FROM curw_fcst.data 
+			SELECT curw_fcst.data.time, curw_fcst.data.value FROM curw_fcst.data 
 				WHERE curw_fcst.data.id = NEW.id 
 					AND curw_fcst.data.fgt = last_run;
 
@@ -41,16 +41,20 @@ BEGIN
 		OPEN curWL;
 			
 		checkWL: LOOP
-			FETCH curWL INTO time, value;
+			FETCH curWL INTO tWL, vWL;
 			IF finished = 1 THEN 
 				LEAVE checkWL;
 			END IF;
 			-- add alert WL to alert table
-			IF value > 1.6 THEN 
+			IF vWL > 1.6 THEN 
+				DELETE FROM `curw_fcst`.`wl_alert`
+				WHERE curw_fcst.wl_alert.hash_id = NEW.id
+                AND curw_fcst.wl_alert.last_run <> last_run;
+            
 				INSERT INTO `curw_fcst`.`wl_alert`
 				(`hash_id`,`time`,`water_level`,`model`,`model_version`,`sim_tag`,`last_run`,`station_name`)
 					VALUES
-					(hash_id, time, value, model, model_version,sim_tag,last_run,station_name);
+					(hash_id, tWL, vWL, model, model_version,sim_tag,last_run,station_name);
 			END IF;
 		END LOOP checkWL;
 		CLOSE curWL;
